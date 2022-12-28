f3fe37c999e2c023846fff070d4f6cbc3673fef6
Olena Avdiunina <elena.avdiunina@gmail.com>
2022-12-28 16:54:45 +0200

    made config edits

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,17 +1,17 @@
-import Navigation from './navigation';
-import MovieCard from './MovieCard';
-import MovieCardSelected from './MovieCardSelected'
-import SelectedMoviesForm from './SelectedMoviesForm';
-import SelectedMoviesSection from './SelectedMoviesSection';
-import ConfirmModal from './ConfirmModal';
-import SocialShare from './SocialShare';
+import Navigation from "./navigation";
+import MovieCard from "./MovieCard";
+import MovieCardSelected from "./MovieCardSelected";
+import SelectedMoviesForm from "./SelectedMoviesForm";
+import SelectedMoviesSection from "./SelectedMoviesSection";
+import ConfirmModal from "./ConfirmModal";
+import SocialShare from "./SocialShare";
 
 export {
-    Navigation,
-    MovieCard,
-    MovieCardSelected,
-    SelectedMoviesForm,
-    SelectedMoviesSection,
-    ConfirmModal,
-    SocialShare
-}
+  Navigation,
+  MovieCard,
+  MovieCardSelected,
+  SelectedMoviesForm,
+  SelectedMoviesSection,
+  ConfirmModal,
+  SocialShare,
+};
--- client/src/const.js
+++ client/src/const.js
@@ -2,7 +2,22 @@ export const MAX_SELECTED_MOVIES = 20;
 export const CONFRIM_TIMEOUT = 3000;
 export const SOCIAL_BUTTON_SIZE = 32;
 export const LOCALES = {
-    ENGLISH: 'en-us',
-    UKRANIAN: 'uk',
-  };
-export const STORAGE_KEY = 'locale';
+  ENGLISH: "en-us",
+  UKRANIAN: "uk",
+};
+export const STORAGE_KEY = "locale";
+
+export const SORT_OPTIONS = [
+  { label: "popularity", value: "popularity" },
+  { label: "release_date", value: "release_date" },
+  { label: "revenue", value: "revenue" },
+  { label: "primary_release_date", value: "primary_release_date" },
+  { label: "original_title", value: "original_title" },
+  { label: "vote_average", value: "vote_average" },
+  { label: "vote_count", value: "vote_count" },
+];
+
+export const SORT_DIRECTION = {
+  DESC: "desc",
+  ASC: "asc",
+};
